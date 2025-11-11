8b163c766f2613c5b83cc05381ee24e0246cc4ef
Samuel Rios <srios050402@gmail.com>
2025-11-10 19:58:00 -0800

    ejs exercice complet

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,25 @@
 "use strict";
-// This is your server file :)
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+const PORT = 3500;
+// Set EJS view engine
+app.set("view engine", "ejs");
+app.set("views", path_1.default.join(__dirname, "views"));
+// ROUTES
+app.get("/", (req, res) => {
+    res.render("home");
+});
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+app.get("/contact", (req, res) => {
+    res.render("contact");
+});
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,26 @@
-// This is your server file :)
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = 3500;
+
+// Set EJS view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
